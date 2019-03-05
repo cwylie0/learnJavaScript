6e7de407b180ed74a0f4f217441662be6c76d4f7
cwylie0 <cwylie0@hotmail.com>
2019-03-05 17:59:16 -0500

    Extra Practice

---
+++ modulesExtraPractice.js
@@ -0,0 +1,4 @@
+const Airplane = require('./modulesExtraPracticeLibrary.js');
+const Airplane2 = require('./modulesExtraPracticeLibrary.js');
+console.log(Airplane.displayAirplane());
+console.log(Airplane2.displayBreed());
