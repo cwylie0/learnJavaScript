0c8c996426d48ba70d16b331315ddc2f3f30c38c
cwylie0 <cwylie0@hotmail.com>
2019-03-14 17:24:04 -0400

    Created subfolder

---
+++ extra practice/modulesExtraPracticeLibrary.js
@@ -0,0 +1,17 @@
+let Airplane = {};
+
+module.exports = {
+  myAirplane: "CloudJet", 
+  displayAirplane: function() {
+    return this.myAirplane;
+  }
+};
+
+let Dog = {};
+
+module.exports = {
+    myBreed: "Chihuahua", 
+    displayBreed: function() {
+      return this.myBreed;
+    }
+  };
