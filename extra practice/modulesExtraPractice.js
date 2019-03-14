6192040b9117ca7ebe9d88442bdb26941ff71978
cwylie0 <cwylie0@hotmail.com>
2019-03-14 17:36:44 -0400

    renamed dir

--- extra practice/modulesExtraPractice.js
+++
@@ -1,4 +0,0 @@
-const Airplane = require('./modulesExtraPracticeLibrary.js');
-const Airplane2 = require('./modulesExtraPracticeLibrary.js');
-console.log(Airplane.displayAirplane());
-console.log(Airplane2.displayBreed());
